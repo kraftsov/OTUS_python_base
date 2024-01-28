5617ca7674abadd43ee4b2f6c7873a67d72b2566
kraft <krafstov@gmail.com>
2024-01-28 19:48:56 +0300

    Homework_08 init

---
+++ homework_08/sql_.sql
@@ -0,0 +1,21 @@
+BEGIN;
+--
+-- Create model Post
+--
+CREATE TABLE "blog_post"
+(
+    "id"        integer      NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title"     varchar(250) NOT NULL,
+    "slug"      varchar(250) NOT NULL,
+    "body"      text         NOT NULL,
+    "publish"   datetime     NOT NULL,
+    "created"   datetime     NOT NULL,
+    "updated"   datetime     NOT NULL,
+    "status"    varchar(2)   NOT NULL,
+    "author_id" integer      NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED
+);
+CREATE INDEX "blog_post_slug_b95473f2" ON "blog_post" ("slug");
+CREATE INDEX "blog_post_author_id_dd7a8485" ON "blog_post" ("author_id");
+CREATE INDEX "blog_post_publish_bb7600_idx" ON "blog_post" ("publish" DESC);
+COMMIT;
+
